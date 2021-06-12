fff0507b32618945db56fadfe19aad8878b788ba
Aaryaman0 <abhageri@uwaterloo.ca>
2021-06-11 22:04:25 -0400

    Added additions using classes

--- database.py
+++ database.py
@@ -1,19 +1,28 @@
 import sqlite3
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from tasks import Tasks
 import sys
 
 con = sqlite3.connect('my.db')
 
 curs = con.cursor()
 
-# curs.execute("""CREATE TABLE Tasks (Name text, type text, deadline text)""")
+# curs.execute("""CREATE TABLE Tasks (name text, type text, deadline text)""")
 
 # curs.execute("INSERT INTO Tasks VALUES ('React Website', 'Sideproject', 'May 12th')")
 
 # con.commit()
 
+task_1 = Tasks('Math247 A5', 'Assignment', 'June 15')
+
+# con.execute("INSERT INTO Tasks VALUES (:Name, :type, :deadline)", {'Name': task_1.name, 'type': task_1.type, 'deadline': task_1.deadline})
+
 curs.execute("SELECT * FROM Tasks WHERE type = 'Sideproject'")
+
+print(curs.fetchall())
+
+curs.execute("SELECT * FROM Tasks WHERE type = :type", {'type': 'Assignment'})
 
 print(curs.fetchall())
 
